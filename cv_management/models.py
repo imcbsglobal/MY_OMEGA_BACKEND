64f2365ac72a05f13e04514bd180cdc154b22cbf
sainudheenp <sainudheen2003@gmail.com>
2025-10-28 11:44:27 +0530

    Add UserCvData model and initial serializers file

--- cv_management/models.py
+++ cv_management/models.py
@@ -1,3 +1,11 @@
 from django.db import models
+from User.models import AppUser
 
 # Create your models here.
+class UserCvData(models.Model) :
+    name = models.CharField(max_length=30)
+    created_at = models.DateField(auto_now_add=True)
+    updated_by = models.ForeignKey(AppUser, verbose_name=("updated User"), on_delete=models.CASCADE)
+
+    def __str__(self):
+        return self.name
