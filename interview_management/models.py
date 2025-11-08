91f6e8343e4d08a10069fe78818cb204a30a0fd4
sainudheenp <sainudheen2003@gmail.com>
2025-11-08 17:54:45 +0530

    Add InterviewManagement model and update settings to include interview_management app

--- interview_management/models.py
+++ interview_management/models.py
@@ -1,2 +1,40 @@
 from django.db import models
+from cv_management.models import UserCvData
+from User.models import AppUser
+import uuid
+
+class InterviewManagement(models.Model):
+    """Stores detailed interview evaluation and results."""
+
+    RESULT_CHOICES =[
+        ('SELECTED','selected'),
+        ("REJECTED",'rejected'),
+        ("PENDING",'pending')
+    ]
+
+    LANGUAGE_CHOICES = [
+    ('English', 'English'),
+    ('Malayalam', 'Malayalam'),
+    ('Tamil', 'Tamil'),
+    ('Hindi', 'Hindi'),
+    ('Other', 'Other'),
+    ]
+
+    id = models.UUIDField(primary_key=True,default=uuid.uuid4 ,editable=False)
+
+    candidate = models.ForeignKey(
+        UserCvData,
+        on_delete=models.CASCADE,
+        related_name='interviews',
+        verbose_name='Candidate'
+    )
+
+    interview_date = models.DateTimeField()
+    interviewer = models.ForeignKey(
+        AppUser,
+        on_delete=models.CASCADE,
+        null=True,
+        related_name='interviews_conducted',
+        verbose_name="Interviewer"
+    )
 
